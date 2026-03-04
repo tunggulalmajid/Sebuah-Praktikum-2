3c107785a16e16e9c1014de818bd5a54638ac161
Eka051 <dianekaraharjo@gmail.com>
2026-03-04 16:24:25 +0700

    feat: add namespace, this keyword && static class

--- Sebuah Praktikum 2/Program.cs
+++ Sebuah Praktikum 2/Program.cs
@@ -1,4 +1,6 @@
-﻿class Rumah
+﻿using PBO_KELAS_C;
+
+class Rumah
 {
     // atribut
     public string warna;
@@ -6,9 +8,9 @@
     public int jumlah_pintu;
     public string alamat; 
 
-    public Rumah(string Warna, int Jumlah_Ruangan, int Jumlah_Pintu, string Alamat)
+    public Rumah(string warna, int Jumlah_Ruangan, int Jumlah_Pintu, string Alamat)
     {
-        warna = Warna;
+        this.warna = warna; // memanggil atributnya sendiri (sbg pembeda antara attribut && parameter)
         jumlah_ruangan = Jumlah_Ruangan;
         jumlah_pintu = Jumlah_Pintu;
         alamat = Alamat;
@@ -22,10 +24,30 @@
         Console.WriteLine($"Jumlah Pintu :{jumlah_pintu} Pintu ");
         Console.WriteLine($"Alamat Rumah :{alamat} ");
     }
+
+    public void InfoRumah()
+    {
+        this.TampilkanInfo(); // memanggil method miliknya sendiri
+        Console.WriteLine("INFO RUMAH BERHASIL DIDAPATKAN");
+    }
 }
 
+namespace PBO_KELAS_C
+{
+   static class Rumah // static class (kelas statik, attribut & method melekat pada class)
+   {
+        public static string warna = "Hijau";
+        public static int jumlah_ruangan = 45;
 
- class Program
+        public static void TampilkanInfo()
+        {
+            Console.WriteLine($"Warna Rumah :{warna} ");
+            Console.WriteLine($"Jumlah Ruangan :{jumlah_ruangan} Ruangan");
+        }
+    }
+}
+
+class Program
 {
     static void Main(string[] args)
     {
@@ -35,6 +57,15 @@
         Rumah rumahMewah = new Rumah("Merah Merona", 10, 5, "bwi"); // ini adalah sebuah objek
         Rumah rumahKecil = new Rumah("Merah abang", 11, 3, "bwi"); // ini adalah sebuah objek
         Rumah rumahBesar = new Rumah("Merah Biru", 190, 4, "Ngawi"); // ini adalah sebuah objek
+        Console.WriteLine(" ");
+
+        //PBO_KELAS_C.Rumah pboC = new PBO_KELAS_C.Rumah();
+        //pboC.TampilkanInfo();
+         
+        /// STATIC CLASS => Memanggil attribut & method tanpa membuat objek
+        PBO_KELAS_C.Rumah.warna = "NAVY"; 
+        PBO_KELAS_C.Rumah.TampilkanInfo();
+        Console.WriteLine(" ");
 
 
         //pemberian value di atribut
@@ -49,6 +80,8 @@
         rumahKecil.TampilkanInfo();
         Console.WriteLine(" ");
         rumahBesar.TampilkanInfo();
+        Console.WriteLine(" ");
+        rumahBesar.InfoRumah();
 
     }
 }
